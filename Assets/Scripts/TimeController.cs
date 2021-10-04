21554004f8103c7257568829071e86fe872e00cb
Luuk Domhof <lukewaffel@gmail.com>
2021-10-04 13:28:32 +0200

    add TimeController

--- Assets/Scripts/TimeController.cs
+++ Assets/Scripts/TimeController.cs
@@ -6,5 +6,35 @@ using ExpPlus.Phariables;
 public class TimeController : MonoBehaviour
 {
 
+    [Header("Events")]
+    [SerializeField]
+    private BoolRepherence pizzaDelivered;
 
+    [Header("Status")]
+    [SerializeField]
+    private FloatRepherence secondsLeft;
+
+    [Header("Config")]
+    [SerializeField]
+    private FloatRepherence secondsPerPizza;
+
+    private void OnEnable()
+    {
+        pizzaDelivered.phariable.SubscribeToOnChangeSignal("triggered", PizzaDelivered);
+    }
+
+    private void OnDisable()
+    {
+        pizzaDelivered.phariable.UnSubscribeFromOnChangeSignal("triggered", PizzaDelivered);
+    }
+
+    private void Update()
+    {
+        secondsLeft.value -= Time.deltaTime;
+    }
+
+    private void PizzaDelivered()
+    {
+        secondsLeft.value = secondsPerPizza.value;
+    }
 }
